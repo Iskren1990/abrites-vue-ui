ce5fc87fbf307c4d16fabbc0ec533da071053241
Iskren Stanchev <istanchev@abrites.com>
2022-02-07 17:17:13 +0200

    refactor/removal of unnecessary dependancy

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,8 @@ import ui from "./abrites-vue-ui/entry";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import Vue3Highlightjs from "./assets/plugins/vue3-highlight-es";
 import "./assets/solarized-light.min.scss";
 import "./assets/global.scss";
 
-createApp(App)
-  .use(ui)
-  .use(store)
-  .use(router)
-  .use(Vue3Highlightjs)
-  .mount("#app");
+createApp(App).use(ui).use(router).use(Vue3Highlightjs).mount("#app");
