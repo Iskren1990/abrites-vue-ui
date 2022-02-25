02e4a40a7b553be4088a72fb3ebe3b42fa2f2f63
Iskren Stanchev <istanchev@abrites.com>
2022-02-25 15:45:12 +0200

    refactor/ outside click -  handle multiple instances

--- src/directives/OutsideClick.ts
+++ src/directives/OutsideClick.ts
@@ -1,19 +1,55 @@
 import { DirectiveBinding } from "vue";
 
-interface ElementWithBinding extends HTMLElement {
-  callback: () => () => unknown;
+class ClickHandler {
+  private _outsideClickStarted = false;
+  private el;
+
+  get id() {
+    return this.el.id;
+  }
+
+  init(el: ElementWithBinding) {
+    this.el = el;
+    document.addEventListener("mousedown", this.outsideClickStarted.bind(this));
+    document.addEventListener("click", this.outsideClickEvent.bind(this));
+  }
+
+  destroy() {
+    document.removeEventListener(
+      "mousedown",
+      this.outsideClickEvent.bind(this)
+    );
+    document.removeEventListener("click", this.outsideClickStarted.bind(this));
+  }
+
+  outsideClickStarted(e: Event) {
+    if (!this.el.contains(e.target as Node)) {
+      this._outsideClickStarted = true;
+    }
+  }
+
+  outsideClickEvent(event: MouseEvent) {
+    if (this.el == null || !this._outsideClickStarted) {
+      this._outsideClickStarted = false; // reset
+      return;
+    }
+    if (
+      !this.el.contains(event.target as Node) &&
+      typeof this.el.callback == "function" &&
+      typeof this.el.callback() == "function"
+    ) {
+      console.log("clicked");
+
+      this.el.callback()();
+    }
+    this._outsideClickStarted = false; // reset
+  }
 }
 
-function outsideClickEvent(el: ElementWithBinding) {
-  return (event: MouseEvent) => {
-    if (
-      !el.contains(event.target as HTMLElement) &&
-      typeof el.callback == "function" &&
-      typeof el.callback() == "function"
-    ) {
-      el.callback()();
-    }
-  };
+let instances: (void | ClickHandler)[] = [];
+
+interface ElementWithBinding extends HTMLElement {
+  callback: () => () => unknown;
 }
 
 // IIFE
@@ -28,11 +64,17 @@ export default (function AbritesOutsideClick() {
     binding: DirectiveBinding<() => () => unknown>
   ): void {
     el.callback = binding.value;
-    document.addEventListener("click", outsideClickEvent(el));
+    el.id = instances.length.toString();
+    const instance = new ClickHandler();
+
+    instances[el.id] = instance;
+    instance.init(el);
   }
 
-  function unmounted(el: ElementWithBinding) {
-    document.removeEventListener("click", outsideClickEvent(el));
+  function unmounted(el) {
+    instances = instances.filter((inst: ClickHandler) =>
+      inst.id == el.id ? inst.destroy() : inst
+    );
   }
 
   return {
