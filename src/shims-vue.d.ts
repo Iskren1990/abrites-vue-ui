de7d83adc9c8f8eebc12083ed6d0370fe0c6ac68
Iskren Stanchev <istanchev@abrites.com>
2022-03-08 11:56:13 +0200

    fix: env typing added https://vitejs.dev/guide/env-and-mode.html

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,14 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_APP_TITLE: string
+  readonly VUE_APP_URL: string
+  readonly VUE_APP_REPO_URL: string
+  readonly NODE_ENV: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
