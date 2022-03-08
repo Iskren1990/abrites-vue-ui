3f694aed44c83d2cc101b8c07f42fa724a980c91
Iskren Stanchev <istanchev@abrites.com>
2022-03-08 14:11:19 +0200

    unnecessary duplicate files removed

--- dev/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <AppLayout />
-</template>
-
-<script lang="ts" setup>
-import { AppLayout } from "./views/shared/index";
-</script>
--- dev/serve.ts
+++
@@ -1,8 +0,0 @@
-import ui from "@/entry";
-import { createApp } from "vue/dist/vue.esm-bundler.js";
-import App from "./App.vue";
-import router from "./router";
-import Vue3Highlightjs from "./assets/plugins/vue3-highlight-es";
-import "./assets/global.scss";
-
-createApp(App).use(ui).use(router).use(Vue3Highlightjs).mount("#app");
